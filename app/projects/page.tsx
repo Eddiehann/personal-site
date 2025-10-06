a8a88d7d9bbcc409212a58b4b97d2baa81b3250a
eddie <jhan45@students.cs.ubc.ca>
2025-10-05 18:02:57 -0700

    Update projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,122 +1,135 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 
 const projects = [
-  {
-    date: 'May 2025',
-    title: 'Personal Portfolio',
-    skill: 'TypeScript, Next.js',
-    desc: 'Front-End Personal Portfolio',
-    detail: [
-      "- Dynamic portfolio written in TypeScript using Next.js framework and deployed with Vercel",
-      "- Implemented responsive layouts to ensure smooth user experience across all screen sizes",
-      "- Styled interactive UI components with Tailwind CSS, incorporating custom animations"
-    ],
-    link: 'https://github.com/Eddiehann/personal-site'
-  },
-  {
-    date: 'June 2024',
-    title: 'AI Assistant Web App',
-    skill: 'HTML, CSS',
-    desc: 'Full-stack Chatbot Powered by GPT-4',
-    detail: [
-      "- Full-stack web application hosting GPT-4's conversational and image analysis features via Azure OpenAI Service",
-      "- Optimized AI prompts for improved output accuracy and formatting",
-      "- Developed a minimalistic UI with HTML and CSS, ensuring a clean, user-friendly design"
-    ],
-    link: 'https://github.com/Eddiehann/ai-assistant'
-  },
-  {
-    date: 'April 2024',
-    title: 'Medieval Sandbox Builder',
-    skill: 'Java',
-    desc: 'Sandbox-style Building Game',
-    detail: [
-      "- Sandbox-style building game developed in Java with a mouse-controlled GUI",
-      "- Implemented JSON-based save/load functionality and logging for debugging",
-      "- Performed testing with JUnit to validate core game interactions"
-    ],
-    link: 'https://github.com/Eddiehann/sandbox'
-  },
-  {
-    date: 'June 2022',
-    title: 'Gift Rush',
-    skill: 'Python, Pygame',
-    desc: 'Roguelike Puzzle Game',
-    detail: [
-      "- Roguelike puzzle game developed in Python using Pygame",
-      "- Developed four-directional character movement mechanics controlled via keyboard inputs",
-      "- Implemented dynamic obstacles and collectible items"
-    ],
-    link: 'https://github.com/Eddiehann/gift-rush'
-  },
+	{
+		date: "July 2025 - August 2025",
+		title: "Study Group Manager",
+		skill: "Javascript, Node.js, SQL",
+		desc: "Full-stack Management Platform",
+		detail: [
+			"- Full-stack platform to create and manage study groups with dynamic user input handling",
+			"- Built a responsive front-end that communicates with RESTful APIs to fetch, update, and render data in real-time, ensuring consistent state and smooth user experience",
+			"- Implemented a Node.js and Express backend with Oracle SQL managing a 15-table relational schema, maintaining data integrity and efficient query performance",
+		],
+		link: "https://github.com/Eddiehann/study-group-manager",
+	},
+	{
+		date: "April 2025 - May 2025",
+		title: "Personal Portfolio",
+		skill: "TypeScript, Next.js",
+		desc: "Front-End Personal Portfolio",
+		detail: [
+			"- Dynamic portfolio written in TypeScript using Next.js framework and deployed with Vercel",
+			"- Implemented responsive layouts to ensure smooth user experience across all screen sizes",
+			"- Styled interactive UI components with Tailwind CSS, incorporating custom animations",
+		],
+		link: "https://github.com/Eddiehann/personal-site",
+	},
+	{
+		date: "May 2024 - June 2024",
+		title: "AI Assistant Web App",
+		skill: "HTML, CSS",
+		desc: "Full-stack Chatbot Powered by GPT-4",
+		detail: [
+			"- Full-stack web application hosting GPT-4's conversational and image analysis features via Azure OpenAI Service",
+			"- Optimized AI prompts for improved output accuracy and formatting",
+			"- Developed a minimalistic UI with HTML and CSS, ensuring a clean, user-friendly design",
+		],
+		link: "https://github.com/Eddiehann/ai-assistant",
+	},
+	{
+		date: "Janurary 2025 - April 2024",
+		title: "Medieval Sandbox Builder",
+		skill: "Java",
+		desc: "Sandbox-style Building Game",
+		detail: [
+			"- Sandbox-style building game developed in Java with a mouse-controlled GUI",
+			"- Implemented JSON-based save/load functionality and logging for debugging",
+			"- Performed testing with JUnit to validate core game interactions",
+		],
+		link: "https://github.com/Eddiehann/sandbox",
+	},
+	{
+		date: "June 2022",
+		title: "Gift Rush",
+		skill: "Python, Pygame",
+		desc: "Roguelike Puzzle Game",
+		detail: [
+			"- Roguelike puzzle game developed in Python using Pygame",
+			"- Developed four-directional character movement mechanics controlled via keyboard inputs",
+			"- Implemented dynamic obstacles and collectible items",
+		],
+		link: "https://github.com/Eddiehann/gift-rush",
+	},
 ];
 
-const sectionLayout=`
+const sectionLayout = `
   grid gap-1 px-3
   border-l-2 border-gray-600 hover:border-gray-400
   transition ease-in-out duration-300
   hover:translate-x-2 hover:-translate-y-1`;
 
 export default function ProjectList() {
+	const [dropDown, setDropDown] = useState(-1);
+	{
+		/* -1 is closed */
+	}
 
-  const [dropDown, setDropDown] = useState(-1); {/* -1 is closed */}
+	return (
+		<div className="grid gap-8 py-2 font-normal">
+			<section className="font-semibold text-gray-100 text-2xl sm:text-3xl sm:py-2">
+				Projects
+			</section>
 
-  return (
-    <div className="grid gap-8 py-2 font-normal">
-
-      <section className="font-semibold text-gray-100 text-2xl sm:text-3xl sm:py-2">
-        Projects
-      </section>
-
-      {projects.map((project, i) => (
-        <section 
-          key={i} 
-          className={sectionLayout} 
-          onClick={() => setDropDown(dropDown === i ? -1 : i)}> {/* set to -1 if already open */}
-
-          <div className="flex justify-between w-full">
-            <div className="font-normal text-sm">{project.date}</div>
-            { dropDown === i && (
-              <a
-                className="flex gap-2 text-sm
+			{projects.map((project, i) => (
+				<section
+					key={i}
+					className={sectionLayout}
+					onClick={() => setDropDown(dropDown === i ? -1 : i)}
+				>
+					{" "}
+					{/* set to -1 if already open */}
+					<div className="flex justify-between w-full">
+						<div className="font-normal text-sm">{project.date}</div>
+						{dropDown === i && (
+							<a
+								className="flex gap-2 text-sm
                   transition ease-in-out fade-right duration-200
                   text-gray-400 hover:text-gray-300"
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="">
-                  Source
-                </div>
-                <Image
-                  src="/external.svg"
-                  alt="external icon"
-                  width={16}
-                  height={16}
-                />
-              </a>
-            )}
-          </div>
-
-          <div>
-            <span className="font-semibold text-gray-300">{project.title}</span>
-            <span className="font-normal"> | {project.skill}</span>
-          </div>
-          <div className="font-normal text-sm">{project.desc}</div>
-
-          { dropDown === i && (
-            <div className="font-normal text-gray-300 text-sm pt-2 transition ease-in-out fade-up">
-              {project.detail.map((point, j) => (
-                <section key={j} className="py-1">{point}</section>
-              ))}
-            </div>
-          )}
-        </section>
-      ))}
-    </div> 
-  )
+								href={project.link}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<div className="">Source</div>
+								<Image
+									src="/external.svg"
+									alt="external icon"
+									width={16}
+									height={16}
+								/>
+							</a>
+						)}
+					</div>
+					<div>
+						<span className="font-semibold text-gray-300">{project.title}</span>
+						<span className="font-normal"> | {project.skill}</span>
+					</div>
+					<div className="font-normal text-sm">{project.desc}</div>
+					{dropDown === i && (
+						<div className="font-normal text-gray-300 text-sm pt-2 transition ease-in-out fade-up">
+							{project.detail.map((point, j) => (
+								<section key={j} className="py-1">
+									{point}
+								</section>
+							))}
+						</div>
+					)}
+				</section>
+			))}
+		</div>
+	);
 }
