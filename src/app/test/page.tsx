13de16b47c42f17524aab053ae4b46063b1d0e3f
eddie <jhan45@students.cs.ubc.ca>
2026-06-04 22:46:07 -0700

    chore: update repo structure

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function TestPage() {
+
+  return (
+    <div className="grid gap-8 py-2 font-normal">
+      <section className="relative w-70 h-70 overflow-hidden">
+        Hi
+      </section>
+    </div>
+  )
+}
